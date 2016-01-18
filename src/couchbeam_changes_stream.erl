cb586ca16708571e3d236d297b055f96d28eb55e
Rafał Słota <roxeon@gmail.com>
2016-01-18 18:15:16 +0100

    Fix compile

--- src/couchbeam_changes_stream.erl
+++ src/couchbeam_changes_stream.erl
@@ -241,7 +241,7 @@ wait_reconnect(#state{parent=Parent,
 decode_data(DataRaw, #state{owner=Owner,
                          ref=Ref,
                          feed_type=continuous,
-                         decoder=DecodeFunNew}=State) ->
+                         decoder=DecodeFunRaw}=State) ->
     DataList = binary:split(DataRaw, <<"\n">>, [global]),
     DecodeFunNew = lists:foldl(fun(Data, DecodeFun) ->
         {incomplete, DecodeFun2} = try case DecodeFun of
